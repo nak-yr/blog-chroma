cde4a9bc76badd643cfe7c3fa45ea0f2a6a36aa4
nak-yr <nkwork874@gmail.com>
2022-01-09 17:58:11 +0900

    issue#10 ヘッダ情報の変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,24 +12,14 @@ const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
-const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
-
 const Header = ({ titlePre = '' }) => {
   const { pathname } = useRouter()
 
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
-        <meta
-          name="description"
-          content="An example Next.js site using Notion for the blog"
-        />
-        <meta name="og:title" content="My Notion Blog" />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={ogImageUrl} />
+        <title>{titlePre ? `${titlePre} |` : ''} Chroma</title>
+        <meta name="description" content="An blog using Next.js and Notion" />
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
